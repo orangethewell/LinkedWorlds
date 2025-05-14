181304e72dd9c9016425009dea0670b0103a6258
orangethewell <67232334+orangethewell@users.noreply.github.com>
2025-05-14 14:18:36 -0300

    style: :coffin: Remove unused vars

--- core/asgi.py
+++ core/asgi.py
@@ -8,12 +8,9 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-
-django_asgi_app = get_asgi_application()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter , URLRouter
